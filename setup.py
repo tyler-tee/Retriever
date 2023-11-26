251be555a5f0e8198872deffef4873ad3dc545d8
tyler-tee <64701075+tyler-tee@users.noreply.github.com>
2023-11-26 10:19:10 -0500

    Increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='RoverRetriever',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/yourusername/RoverRetriever',
     author='Tyler Talaga',
     author_email='ttalaga@wgu.edu',
@@ -10,8 +10,6 @@ setup(
     packages=['RoverRetriever'],
     install_requires=['requests >= 2.25.1'],
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
