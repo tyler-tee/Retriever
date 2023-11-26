cde2c6544b138f96b883faa1f651da5786ee3399
tyler-tee <64701075+tyler-tee@users.noreply.github.com>
2023-11-26 10:37:32 -0500

    Update description

--- setup.py
+++ setup.py
@@ -1,19 +1,28 @@
 from setuptools import setup
+from os import path
+
+currrent_direct = path.abspath(path.dirname(__file__))
+with open(path.join(currrent_direct, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='RoverRetriever',
-    version='0.1.1',
-    url='https://github.com/yourusername/RoverRetriever',
+    version='0.1.4',
+    url='https://github.com/tyler-tee/RoverRetriever',
     author='Tyler Talaga',
     author_email='ttalaga@wgu.edu',
-    description='A lightweight Python wrapper for the Dog CEO API',
     packages=['RoverRetriever'],
     install_requires=['requests >= 2.25.1'],
+    license='MIT',
     classifiers=[
-        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
+    description='RoverRetriever is a lightweight Python wrapper written to facilitate interaction with the Dog CEO API.',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
