3c045191a59f27a91fd511b778b4e77a2dec564a
tyler-tee <64701075+tyler-tee@users.noreply.github.com>
2023-11-26 13:01:41 -0500

    Increment version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(currrent_direct, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='RoverRetriever',
-    version='0.1.4',
+    version='0.1.5',
     url='https://github.com/tyler-tee/RoverRetriever',
     author='Tyler Talaga',
     author_email='ttalaga@wgu.edu',
